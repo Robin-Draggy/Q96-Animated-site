b365f1f705cd3b789f6a30f8692672819bee19b7
Abdullah Al Mamun <abdullahmn3399@gmail.com>
2026-05-04 00:20:20 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Projects from "./pages/Projects"
+import Agency from "./pages/Agency"
+
+function App() {
+
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/agency" element={<Agency />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
